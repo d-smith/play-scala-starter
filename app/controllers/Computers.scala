d681baaa7466152276bc335a40a9088b0cbde405
d-smith <doug.smith.mail@gmail.com>
2015-04-07 07:17:12 -0600

    Added simple implementation of the list method

--- app/controllers/Computers.scala
+++ app/controllers/Computers.scala
@@ -1,7 +1,13 @@
 package controllers
 
 import play.api.mvc.{Controller, Action}
+import play.api.libs.json.Json
 
 object Computers extends Controller {
-  val list = Action { NotImplemented }
+  val list = Action {
+    Ok(Json.obj(
+        "name" -> "Sun Sparcstation",
+        "manufacturer" -> "Sun"
+    ))      
+  }
 }
