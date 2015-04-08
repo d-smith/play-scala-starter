622c0ce2f71fe8feae72c0e450e941ea355ff221
d-smith <doug.smith.mail@gmail.com>
2015-04-08 07:45:25 -0600

    Added details with uri param plus better json serialization

--- app/controllers/Computers.scala
+++ app/controllers/Computers.scala
@@ -1,13 +1,29 @@
 package controllers
 
 import play.api.mvc.{Controller, Action}
-import play.api.libs.json.Json
+import play.api.libs.json._
+
+case class ComputerInfo(name: String, manufacturer: String)
+
+object ComputerInfo {
+    implicit val implicitComputerInfoWrites = new Writes[ComputerInfo] {
+        def writes(comp: ComputerInfo) : JsValue = {
+            Json.obj(
+                "name" -> comp.name,
+                "manufacturer" -> comp.manufacturer
+            )
+        }
+    }
+}
 
 object Computers extends Controller {
   val list = Action {
-    Ok(Json.obj(
-        "name" -> "Sun Sparcstation",
-        "manufacturer" -> "Sun"
-    ))      
+      val comp = ComputerInfo("sparcstation 1", "sun")
+      Ok(Json.toJson(comp))
+  }
+  
+  def details(name: String) = Action {
+      val comp = ComputerInfo(name, "AcmeComputer")
+      Ok(Json.toJson(comp))
   }
 }
