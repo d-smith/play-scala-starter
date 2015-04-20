9d8e6fd43f09bc2f7a55b592c3acd0f095b6c18a
d-smith <doug.smith.mail@gmail.com>
2015-04-20 06:18:32 -0600

    Added func test with POST

--- test/ComputerAppSpec.scala
+++ test/ComputerAppSpec.scala
@@ -1,18 +1,47 @@
+import models.ComputerInfo
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 import play.api.test.Helpers._
-import play.api.test.{FakeRequest, WithApplication, PlaySpecification}
+import play.api.test.{FakeHeaders, FakeRequest, WithApplication, PlaySpecification}
+import play.api.libs.json.Json
 
 @RunWith(classOf[JUnitRunner])
 class ComputerAppSpec extends PlaySpecification {
 
-    "Computer application" should {
-      "return a 404 when asking for a computer that isn't known" in new WithApplication {
-        route(FakeRequest(GET, "/computers/foo")) match {
-          case Some(response) => status(response) must equalTo(NOT_FOUND)
-          case None => failure
-        }
+  "Computer application" should {
+    "return a 404 when asking for a computer that isn't known" in new WithApplication {
+      route(FakeRequest(GET, "/computers/foo")) match {
+        case Some(response) => status(response) must equalTo(NOT_FOUND)
+        case None => failure
       }
     }
+
+    "accept a computer definition posted to it" in new WithApplication {
+      val compInfo = ComputerInfo("a-computer", "b")
+      val compJson = Json.toJson(compInfo)
+      val request = FakeRequest(
+        POST,
+        "/computers",
+        FakeHeaders(
+          Seq("Content-type" -> Seq("application/json"))
+        ),
+        compJson)
+
+      route(request) match {
+        case Some(response) => status(response) must equalTo(OK)
+        case None => failure
+      }
+
+
+      route(FakeRequest(GET, "/computers/a-computer")) match {
+        case Some(response) => status(response) must equalTo(OK)
+        case None => failure
+      }
+
+      //TODO - make sure the comp we got back has the values we expect
+    }
+
+
+  }
 }
