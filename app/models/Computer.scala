8f2da86e378eaf8eaddcf2b7f642a689c4f1387b
d-smith <doug.smith.mail@gmail.com>
2015-04-26 06:42:43 -0600

    Added trait and in memory version of a computer repository

--- app/models/Computer.scala
+++ app/models/Computer.scala
@@ -5,7 +5,26 @@ import play.api.libs.functional.syntax._
 
 case class Computer(name: String, manufacturer: String)
 
+trait ComputerCatalog {
+  def list: Iterable[Computer]
 
+  def create(name: String, manufacturer: String) : Option[Computer]
+
+  def get(name: String) : Option[Computer]
+
+}
+
+object InMemoryCatalog extends ComputerCatalog {
+  val computerMap = scala.collection.mutable.HashMap.empty[String, Computer]
+
+  override def list: Iterable[Computer] = computerMap.values.seq
+
+  override def get(name: String): Option[Computer] = computerMap.get(name)
+
+  override def create(name: String, manufacturer: String): Option[Computer] =
+      computerMap.put(name, Computer(name, manufacturer))
+  
+}
 
 object Computer {
   implicit val implicitComputerInfoWrites = new Writes[Computer] {
